9534a7ade9a636df75cf7f9e4152af45147af46d
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2020-10-04 18:27:38 +0900

    基本の型と型推論

--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("入力値が正しくありません");
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
-const number1 = 5;
+let number1: number;
+number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+let resultPhrase = "Result: ";
+// resultPhrase = 5;
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
